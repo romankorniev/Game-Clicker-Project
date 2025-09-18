5a5c71f0d5b738eb59508b9a1e484c9b8dca3243
Admin <korniev.roman@gmail.com>
2025-09-19 00:15:11 +0300

    Game Clicker Project

---
+++ js/main.js
@@ -0,0 +1,86 @@
+const startGameBtn = document.getElementById('startGameBtn')
+const gameContainer = document.getElementById('game-container')
+
+let score = 0
+let timer = 10
+let gameActive = false
+let interval 
+
+startGameBtn.addEventListener('click', startGame)
+
+function startGame(){
+  startGameBtn.style.display =  'none'
+  gameActive = true
+  score = 0
+  timer = parseInt(document.getElementById('duration').value) || 10
+  
+  updateScore()
+  updateTimer()
+
+  interval = setInterval(() => {
+    if(timer > 0){
+      timer--
+      updateTimer()
+    }else{
+      endGame()
+    }
+  }, 1000)
+
+  createCircle()
+}
+
+function endGame(){
+  gameActive = false
+  clearInterval(interval)
+  clearGameContainer()
+  startGameBtn.style.display = 'block'
+}
+
+function clearGameContainer(){
+  gameContainer.lastElementChild.remove()
+}
+
+function createCircle(){
+
+  const circle = document.createElement('div')
+  circle.classList.add('circle')
+  const {x, y} = getRandomPosition()
+  circle.style.left = `${x}px` 
+  circle.style.top = `${y}px`
+  gameContainer.appendChild(circle)
+
+  circle.addEventListener('click', () => {
+    if(gameActive){
+      score++
+      updateScore()
+      circle.remove()
+      createCircle()
+    }
+  })
+
+  setTimeout(() => {
+    if (circle.parentNode) {
+      circle.remove()
+      score--
+      updateScore()
+      if (gameActive) createCircle()
+    }
+  }, 700)
+}
+
+function getRandomPosition(){
+  const maxX = gameContainer.clientWidth - 50
+  const maxY = gameContainer.clientHeight - 50
+  const x = Math.floor(Math.random() * maxX)
+  const y = Math.floor(Math.random() * maxY)
+  return {x, y}
+}
+
+function updateScore(){
+  document.getElementById('score').textContent = `Score: ${score}`
+}
+
+function updateTimer(){
+  document.getElementById('timer').textContent = `Time: ${timer}`
+}
+
